43c0b2e51594bf71e1265b60b45ccf70abc0e8fa
Yousan_O <yousan@l2tp.org>
2016-11-24 23:06:26 +0900

    Automatically commit

--- example3.php
+++ example3.php
@@ -1,12 +1,17 @@
 <?php
 /**
+ * Hello
+ *
  * @package hoge
  */
 
+/**
+ * Yo man
+ */
 function hogehoge() {
 	echo 'hoge';
 	$i_am = 'hungry';
-	if ('hungry' === $i_am) {
+	if ( 'hungry' === $i_am ) {
 		echo 'give u some chocolates.';
 	} else {
 		echo 'u must be a pig.';
